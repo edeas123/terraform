4375556caecfe110d84d650acfd53fc7cbfbd90c
edeas123 <edeas123@gmail.com>
2018-12-01 01:41:47 -0500

    add lifecycle policy to ecr repo

--- dev/blogops/main.tf
+++ dev/blogops/main.tf
@@ -2,3 +2,25 @@
 resource "aws_ecr_repository" "blogextractor" {
 	name = "blogextractor"
 }
+
+resource "aws_ecr_lifecycle_policy" "blogextractor_lifecyle_policy" {
+	repository = "${aws_ecr_repository.blogextractor.name}"
+	policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Retain at most 1 image",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
