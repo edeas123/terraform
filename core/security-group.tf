73e076dd5ca24e50e82d69fbd1af754049bb6cdc
edeas123 <edeas123@gmail.com>
2018-12-01 00:34:39 -0500

    put security groups in one place

---
+++ core/security-group.tf
@@ -0,0 +1,84 @@
+# create a security group for the web servers
+resource "aws_security_group" "web-sg" {
+	name = "web-sg"
+	description = "Security group for the web servers"
+	vpc_id = "${var.vpc_id}"
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allow all inbound SSH connections"
+	}
+
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allow HTTP connections on port 80"
+	}
+
+	 egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allows all outbound traffic from the instance"
+	 }
+}
+
+resource "aws_security_group" "playground-sg" {
+	name = "playground-sg"
+	description = "Security group for the playground instances"
+	vpc_id = "${var.vpc_id}"
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allow all inbound SSH connections"
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allows all outbound traffic from the instance"
+	}
+}
+
+# create a security group for the jenkins server
+resource "aws_security_group" "jenkins-sg" {
+	name = "jenkins-sg"
+	description = "Security group for the jenkins servers"
+	vpc_id = "${var.vpc_id}"
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allow all inbound SSH connections"
+	}
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allow HTTP connections on port 8080 used for the Jenkins UI"
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allows all outbound traffic from the instance"
+	}
+}
+
