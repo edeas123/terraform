2513745a8cdee190a21e21c4445a93ce3eb4d499
edeas123 <edeas123@gmail.com>
2018-12-01 19:14:53 -0500

    setup blogextractor infra

--- dev/blogops/main.tf
+++ dev/blogops/main.tf
@@ -24,3 +24,94 @@ resource "aws_ecr_lifecycle_policy" "blogextractor_lifecyle_policy" {
 }
 EOF
 }
+
+# TODO: two spot instances, 1 ASG with 1 additional instance, 1lb
+# TODO: Use fleet instead
+resource "aws_spot_instance_request" "blogextractor-spot-1" {
+	ami	= "ami-0f9cf087c1f27d9b1"
+	instance_type = "t2.micro"
+	spot_type = "one-time"
+	key_name = "deployer"
+	availability_zone = "us-east-1a"
+	vpc_security_group_ids = [
+		"${data.terraform_remote_state.core.blogextractor-sg-id}"
+	]
+	wait_for_fulfillment = true
+	timeouts {
+		create = "3m"
+	}
+	tags {
+		Name = "blogextractor-spot-1"
+	}
+}
+
+resource "aws_spot_instance_request" "blogextractor-spot-2" {
+	ami	= "ami-0f9cf087c1f27d9b1"
+	instance_type = "t2.micro"
+	spot_type = "one-time"
+	key_name = "deployer"
+	availability_zone = "us-east-1a"
+	vpc_security_group_ids = [
+		"${data.terraform_remote_state.core.blogextractor-sg-id}"
+	]
+	wait_for_fulfillment = true
+	timeouts {
+		create = "3m"
+	}
+	tags {
+		Name = "blogextractor-spot-2"
+	}
+}
+
+# create an application load balancer
+resource "aws_alb" "blogextractor-alb" {
+	name = "blogextractor-alb"
+	internal = false
+	load_balancer_type = "application"
+	ip_address_type  = "ipv4"
+
+	subnets = ["${data.aws_subnet_ids.vpc_subnets.ids}"]
+	security_groups = [
+		"${data.terraform_remote_state.core.blogextractor-sg-id}"
+	]
+}
+
+# create an application load balancer listener 
+resource "aws_alb_listener" "blogextractor-alb-listener" {
+	load_balancer_arn = "${aws_alb.blogextractor-alb.arn}"
+	port = 5000
+	protocol = "HTTP"
+
+	default_action {
+    	type             = "forward"
+    	target_group_arn = "${aws_alb_target_group.blogextractor-target-group.arn}"
+  	}
+}
+
+# create the application load balancer target group
+resource "aws_alb_target_group" "blogextractor-target-group" {
+	name = "blogextractor-target-group"
+	port = 5000
+	protocol = "HTTP"
+	vpc_id = "vpc-6f3d7608"
+}
+
+# attach the two webserver instances to the target group
+resource "aws_alb_target_group_attachment" "blogextractor-1-target-group-instance" {
+  target_group_arn = "${aws_alb_target_group.blogextractor-target-group.arn}"
+  target_id        = "${aws_spot_instance_request.blogextractor-spot-1.spot_instance_id}"
+  port             = 5000
+}
+resource "aws_alb_target_group_attachment" "blogextractor-2-target-group-instance" {
+  target_group_arn = "${aws_alb_target_group.blogextractor-target-group.arn}"
+  target_id        = "${aws_spot_instance_request.blogextractor-spot-2.spot_instance_id}"
+  port             = 5000
+}
+
+output "blogextractor-spot-1-ip" {
+	value = "${aws_spot_instance_request.blogextractor-spot-1.public_ip}"
+}
+
+output "blogextractor-spot-2-ip" {
+	value = "${aws_spot_instance_request.blogextractor-spot-2.public_ip}"
+}
