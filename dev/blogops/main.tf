d59794a72699303957763f7e5ee53844a954beb5
edeas123 <edeas123@gmail.com>
2018-12-21 06:07:19 -0500

    add a route53 recordset

--- dev/blogops/main.tf
+++ dev/blogops/main.tf
@@ -1,4 +1,17 @@
 # TODO: two spot instances, 1 ASG with 1 additional instance, 1lb
+
+resource "aws_route53_record" "blogextractor-alias-record" {
+  zone_id = "${data.aws_route53_zone.primary-domain.zone_id}"
+  name    = "extractors.${data.aws_route53_zone.primary-domain.name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_alb.blogextractor-alb.dns_name}"
+    zone_id                = "${aws_alb.blogextractor-alb.zone_id}"
+    evaluate_target_health = false
+  }
+}
+
 output "blogextractor-1-ip" {
 	value = "${aws_instance.blogextractor-1.public_ip}"
 }
