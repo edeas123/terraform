4d035c8699ca6633b2b341ddf9fb3b756d37c050
edeas123 <edeas123@gmail.com>
2019-01-25 01:07:34 -0500

    add sg for rancher

--- core/security-group.tf
+++ core/security-group.tf
@@ -137,6 +137,14 @@ resource "aws_security_group" "rancher-container-host-sg" {
 		description = "Allow communication between container hosts"
 	}
 
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+		description = "Allow HTTP connections on port 80 used for the UI"
+	}
+
 	egress {
 		from_port = 0
 		to_port = 0
@@ -150,6 +158,91 @@ resource "aws_security_group" "rancher-container-host-sg" {
   }
 }
 
+resource "aws_security_group" "rancher-ctl-host-sg" {
+	name = "rancher-ctl-host-sg"
+	description = "Security group for the rancher control host"
+  	vpc_id = "${data.terraform_remote_state.core.default-vpc-id}"
+
+	ingress {
+		from_port = 9345
+		to_port = 9345
+		protocol = "tcp"
+		self = true
+		description = "Allow communication between hosts on HA"
+	}
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		security_groups = ["${aws_security_group.rancher-ctl-host-alb-sg.id}"]
+		description = "Allow HTTP connections on port 8080 used for the UI"
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allows all outbound traffic from the instance"
+	}
+
+  tags {
+      Name = "rancher-ctl-host"
+  }
+}
+
+resource "aws_security_group" "rancher-ctl-rds-sg" {
+	name = "rancher-ctl-rds-sg"
+	description = "Security group for the rancher database"
+  	vpc_id = "${data.terraform_remote_state.core.default-vpc-id}"
+
+	ingress {
+		from_port = 3306
+		to_port = 3306
+		protocol = "tcp"
+    security_groups = ["${aws_security_group.rancher-ctl-host-sg.id}"]
+		description = "Allow communication on the database port from only the rancher-ctl-host"
+	}
+
+  tags {
+      Name = "rancher-ctl-rds"
+  }
+}
+
+resource "aws_security_group" "rancher-ctl-host-alb-sg" {
+	name = "rancher-ctl-host-alb-sg"
+	description = "Security group for the rancher host alb"
+  	vpc_id = "${data.terraform_remote_state.core.default-vpc-id}"
+
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+    cidr_blocks = "${local.trusted-cidrs}"
+		description = "Allow HTTP connections on port 80 used for the UI"
+	}
+
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+		description = "Allow HTTP connections on port 80 from container hosts"
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "Allows all outbound traffic from the alb"
+	}
+
+  tags {
+      Name = "rancher-ctl-host-alb"
+  }
+}
 output "jenkins-sg-id" {
   value = "${aws_security_group.jenkins-sg.id}"
 }
@@ -172,4 +265,15 @@ output "blogextractor-sg-id" {
 
 output "rancher-container-host-sg-id" {
   value = "${aws_security_group.rancher-container-host-sg.id}"
+}
+
+output "rancher-ctl-host-sg-id" {
+  value = "${aws_security_group.rancher-ctl-host-sg.id}"
+}
+output "rancher-ctl-rds-sg-id" {
+  value = "${aws_security_group.rancher-ctl-rds-sg.id}"
+}
+
+output "rancher-ctl-host-alb-sg-id" {
+  value = "${aws_security_group.rancher-ctl-host-alb-sg.id}"
 }
