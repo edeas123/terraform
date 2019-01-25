c298e7e95984728762c380fad3e578e857844d7c
edeas123 <edeas123@gmail.com>
2019-01-25 01:07:07 -0500

    add more cidr, and add ubuntu-ami

--- dev/jenkins/vars.tf
+++ dev/jenkins/vars.tf
@@ -8,6 +8,11 @@ variable "domain" {
 
 locals {
 	trusted-cidrs = [
-		"${var.home-mac-cidr}"
+		"${var.home-cidr}",
+    "${var.office-cidr}"
 	]
+}
+
+variable "ubuntu-ami" {
+  default = "ami-0653e888ec96eab9b"
 }
