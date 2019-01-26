50b1750654f3bedb5b5974aee419153fe8e7e6be
edeas123 <edeas123@gmail.com>
2019-01-26 14:17:01 -0500

    playing with spot fleet requests

--- playground/main.tf
+++ playground/main.tf
@@ -17,6 +17,42 @@ resource "aws_spot_instance_request" "playground-spot-1" {
 	}
 }
 
+resource "aws_spot_fleet_request" "playground-fleet-1" {
+  iam_fleet_role = "arn:aws:iam::524932011419:role/aws-service-role/spotfleet.amazonaws.com/AWSServiceRoleForEC2SpotFleet"
+  launch_specification = {
+    ami	= "ami-0653e888ec96eab9b"
+    instance_type = "m4.large"
+    key_name = "deployer"
+    availability_zone = "${data.aws_availability_zones.zones.names[0]}"
+    vpc_security_group_ids = [
+      "${data.terraform_remote_state.core.playground-sg-id}",
+      "${data.terraform_remote_state.core.ssh-sg-id}"
+	  ]
+    tags {
+      Name = "playground-spot-1"
+    }
+  }
+  launch_specification = {
+    ami	= "ami-0653e888ec96eab9b"
+    instance_type = "m4.large"
+    key_name = "deployer"
+    availability_zone = "${data.aws_availability_zones.zones.names[2]}"
+    vpc_security_group_ids = [
+      "${data.terraform_remote_state.core.playground-sg-id}",
+      "${data.terraform_remote_state.core.ssh-sg-id}"
+    ]
+    tags {
+      Name = "playground-spot-1"
+    }
+  }
+  target_capacity = 3
+  allocation_strategy = "diversified"
+	wait_for_fulfillment = true
+	timeouts = {
+		create = "3m"
+	}  
+}
+
 output "playground-spot-1-ip" {
 	value = "${aws_spot_instance_request.playground-spot-1.public_ip}"
 }
