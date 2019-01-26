51e92c96ceaca3bc57c44199f3aeb37cb1917bf2
edeas123 <edeas123@gmail.com>
2019-01-26 17:40:22 -0500

    use launch configuration and spot instances for the rancher containers

--- dev/rancher/rancher-container/main.tf
+++ dev/rancher/rancher-container/main.tf
@@ -14,16 +14,19 @@ data "aws_ami" "rancher-container-host-ami" {
   owners = ["self"]
 }
 
-resource "aws_launch_template" "rancher-container-host-lt" {
-  name = "rancher-container-host-lt"
-  description = "Launch template for the rancher controller host instances"
+resource "aws_launch_configuration" "rancher-container-host" {
+  name_prefix = "rancher-container-host-"
   image_id = "${data.aws_ami.rancher-container-host-ami.id}"
-  instance_type = "t2.micro"
+  instance_type = "m4.large"
   key_name = "deployer"
-	vpc_security_group_ids = [
+	security_groups = [
 		"${data.terraform_remote_state.core.rancher-container-host-sg-id}",
 		"${data.terraform_remote_state.core.ssh-sg-id}"
 	]
+  lifecycle = {
+    create_before_destroy = true
+  }
+  spot_price = 0.18
 	user_data = "${base64encode(file("make-rancher-container-host.sh"))}"
 }
 
@@ -32,9 +35,17 @@ resource "aws_autoscaling_group" "rancher-container-host-asg" {
   max_size = 4
   desired_capacity = 2
   min_size = 1
-  launch_template = {
-    name = "${aws_launch_template.rancher-container-host-lt.name}"
-  }
+  launch_configuration = "${aws_launch_configuration.rancher-container-host.name}"
   vpc_zone_identifier = ["${data.aws_subnet_ids.vpc_subnets.ids}"]
   termination_policies = ["OldestInstance"]
+  tags = [
+    {
+      key = "Name",
+      value = "rancher-container-host",
+      propagate_at_launch = true
+    }
+  ]
+  lifecycle = {
+    create_before_destroy = true
+  }
 }
