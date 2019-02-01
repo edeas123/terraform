f26e37ed944469932845c7433f557e7a062f673a
edeas123 <edeas123@gmail.com>
2019-01-31 19:10:02 -0500

    update remote backend key

--- dev/blogextractor/remote-backend.tf
+++ dev/blogextractor/remote-backend.tf
@@ -1,7 +1,7 @@
 terraform {
 	backend "s3" {
 		bucket = "mybytesni-tfstate"
-		key = "blogops.tfstate"
+		key = "blogextractor.tfstate"
 		region = "us-east-2"
 		dynamodb_table = "terraform-locking-table"
 	}
