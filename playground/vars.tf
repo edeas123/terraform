e1a1bae8730dd0079b1eb06a675494c70b69d40d
edeas123 <edeas123@gmail.com>
2019-02-05 20:14:40 -0500

    use symbolic link for common files

--- playground/vars.tf
+++ playground/vars.tf
@@ -1,14 +1,1 @@
-variable "region" {
-	default = "us-east-2"
-}
-
-variable "domain" {
-	default = "mybytesni.com"
-}
-
-locals {
-	trusted-cidrs = [
-		"${var.home-cidr}",
-    "${var.office-cidr}"
-	]
-}
+../common/vars.tf
