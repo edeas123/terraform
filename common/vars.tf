2c92895b4e4f991aabf482a626f039a0601c82d2
edeas123 <edeas123@gmail.com>
2019-02-15 08:09:15 -0500

    move variable's defaults to secrets

--- common/vars.tf
+++ common/vars.tf
@@ -6,6 +6,12 @@ variable "domain" {
 	default = "mybytesni.com"
 }
 
+variable "home-cidr" {}
+
+variable "office-cidr" {}
+
+variable "vpn-cidr" {}
+
 locals {
 	trusted-cidrs = [
 		"${var.home-cidr}",
