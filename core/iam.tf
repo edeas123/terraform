49f0b3c03f9d2605d639247f58969b7926e4c8bb
edeas123 <edeas123@gmail.com>
2019-02-21 21:04:18 -0500

    move s3access iam policy to core

--- core/iam.tf
+++ core/iam.tf
@@ -15,3 +15,42 @@ resource "aws_iam_user_policy_attachment" "circleci-ecr-policy-attachment" {
   user       = "${aws_iam_user.circleci.name}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
+
+# create a policy document for the role
+# instead of using a policy document, it can used directly in
+# the role using terraform heredoc syntax
+data "aws_iam_policy_document" "s3access-role-policy" {
+	statement {
+		actions = ["sts:AssumeRole"]
+		principals {
+			type 		= "Service"
+			identifiers = ["ec2.amazonaws.com"]
+		}
+	}
+}
+
+# set an aws iam role with accompanying policy for ec2 
+resource "aws_iam_role" "s3access-role" {
+	name = "s3access-role"
+	description = "Allows EC2 instances to call AWS S3 service on your behalf."
+ 	assume_role_policy = "${data.aws_iam_policy_document.s3access-role-policy.json}"
+}
+
+# attach a managed policy to the created role, for full access to s3
+resource "aws_iam_policy_attachment" "s3access-policy-attachment" {
+  name       = "s3access-policy-attachment"
+  roles      = ["${aws_iam_role.s3access-role.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# enable the ec2 instance to use that role and manually test
+# since internal aws uses the same name for the role and profile
+# its best to do the same
+resource "aws_iam_instance_profile" "s3access-role" {
+	name = "s3access-role"
+	role = "${aws_iam_role.s3access-role.name}"
+}
+
+output "s3access-role-iam-instance-profile-name" {
+  value = "${aws_iam_instance_profile.s3access-role.name}"
+}
